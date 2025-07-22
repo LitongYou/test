c37538cd56d9cf83e51e121a59d090fb1e2fdd3c
Daniel <youlitong1994@live.com>
2025-07-22 09:16:56 +0800

    Update quick_start.py

--- quick_start.py
+++ quick_start.py
@@ -1,11 +1,11 @@
-from oscopilot import FridayAgent
-from oscopilot import ToolManager
-from oscopilot import FridayExecutor, FridayPlanner, FridayRetriever
-from oscopilot.utils import setup_config, setup_pre_run
+from strata import StrataAgent
+from strata import ToolManager
+from strata import StrataExecutor, StrataPlanner, StrataRetriever
+from strata.utils import setup_config, setup_pre_run
 
 args = setup_config()
 if not args.query:
     args.query = "Copy any text file located in the working_dir/document directory that contains the word 'agent' to a new folder named 'agents' "
 task = setup_pre_run(args)
-agent = FridayAgent(FridayPlanner, FridayRetriever, FridayExecutor, ToolManager, config=args)
+agent = StrataAgent(StrataPlanner, StrataRetriever, StrataExecutor, ToolManager, config=args)
 agent.run(task=task)
